1331135895f92eddf64edf123fefab5e97458fcd
karson <karsonam458@gmail.com>
2026-02-08 18:46:54 +0600

    todo app final

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
@@ -7,3 +7,39 @@ def addTask(req):
     task=req.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+
+def mark_as_done(req,pk):
+    # pk=pk =one is field name other is dyanmic data pass from pk
+    task=get_object_or_404(Task,pk=pk)
+    task.is_completed=True
+    task.save()
+    
+
+    return redirect('home')
+
+
+def delete_task(req,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect('home')
+
+def mark_as_undone(req,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
+
+def edit_task(req,pk):
+    get_task=get_object_or_404(Task,pk=pk)
+    if req.method == 'POST':
+        new_task=req.POST['task']
+        get_task.task=new_task
+        get_task.save()
+        return redirect('home')
+
+    else: 
+        context={
+            'get_task':get_task
+        }   
+        return render(req,'edit_task.html',context)
--- todo_main/views.py
+++ todo_main/views.py
@@ -3,8 +3,10 @@ from todo.models import Task
 
 def home(req):
     task=Task.objects.filter(is_completed=False).order_by('updated_at')
+    task_completed=Task.objects.filter(is_completed=True)
     context={
-        'task':task
+        'task':task,
+        'task_completed':task_completed
     }
 
     return render(req,'home.html',context)
